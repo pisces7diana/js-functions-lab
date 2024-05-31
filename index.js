36c30074c335314e3cb2635df15151e28b463eb1
pisces7diana <email7diana@gmail.com>
2024-05-30 17:38:26 -0700

    Intro to JS Functions lab

---
+++ index.js
@@ -0,0 +1,13 @@
+// write a function that prints your name
+function sayMyName (x) {
+    console.log('My name is' + " " + x)
+}
+
+sayMyName('diana')
+
+// or 
+function name() {
+    console.log('diana')
+}
+
+name()
